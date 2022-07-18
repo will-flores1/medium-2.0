2e0241e13dcb38215a093a3800fad58a218a58bf
Will Flores <willflores1221@gmail.com>
2022-07-18 07:28:59 -0400

    Fix ref-warnings

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,14 +6,16 @@ function Header() {
 	return (
 		<header className="flex justify-between p-5 max-w-7xl mx-auto">
 			<div className="flex items-center space-x-5">
-				<Link href="/">
-					<Image
-						className="w-44 object-contain cursor-pointer"
-						src={MediumLogo}
-						width={120}
-						height={32}
-						alt=""
-					/>
+				<Link href="/" passHref>
+					<a>
+						<Image
+							className="w-44 object-contain cursor-pointer"
+							src={MediumLogo}
+							width={120}
+							height={32}
+							alt=""
+						/>
+					</a>
 				</Link>
 				<div className="hidden md:inline-flex items-center space-x-5">
 					<h3>About</h3>
